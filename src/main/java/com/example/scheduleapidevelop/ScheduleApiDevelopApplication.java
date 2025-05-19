cbb6cbd4701ef253bde2f8a3e9d63a835c61397e
saevit <woosaevit@gmail.com>
2025-05-19 15:35:47 +0900

    init commit

---
+++ src/main/java/com/example/scheduleapidevelop/ScheduleApiDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduleapidevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleApiDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApiDevelopApplication.class, args);
+    }
+
+}
